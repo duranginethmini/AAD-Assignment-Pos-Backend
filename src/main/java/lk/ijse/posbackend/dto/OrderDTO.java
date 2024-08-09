680a1c6b1da758a77a097d0bdfb3a736033de4d3
duranginethmini <durangineth02@gmail.com>
2024-08-09 13:27:23 +0530

    Order CRUD completed

--- src/main/java/lk/ijse/posbackend/dto/OrderDTO.java
+++ src/main/java/lk/ijse/posbackend/dto/OrderDTO.java
@@ -3,6 +3,8 @@ package lk.ijse.posbackend.dto;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,12 +14,14 @@ import java.time.LocalDate;
 
 public class OrderDTO {
     private String orderId;
-    private String cusId;
-    private String itemCode;
-    private LocalDate orderDate;
-    private int orderQty;
-    private double cash;
-    private double discount;
-    private double balance;
+    private String orderDate;
+    private String custId;
+    private List<ItemDTO> items = new ArrayList<>();
+    private Double total;
+    private String discount;
+    private Double subTotal;
+    private Double cash;
+    private Double balance;
+
 
 }
